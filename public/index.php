7564d82fe72d2e031c4bd6a17e3f5cb194f21322
Роман Бушуев <bushuev@benefitty.ru>
2024-03-25 09:57:01 +0400

    chore: fix directories
    Исправил структуру каталогов.

--- public/index.php
+++ public/index.php
@@ -5,7 +5,7 @@ use Illuminate\Http\Request;
 define('LARAVEL_START', microtime(true));
 
 // Determine if the application is in maintenance mode...
-if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
+if (file_exists($maintenance = __DIR__ . '/../storage/framework/maintenance.php')) {
     require $maintenance;
 }
 
@@ -13,5 +13,5 @@ if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php'))
 require __DIR__.'/../vendor/autoload.php';
 
 // Bootstrap Laravel and handle the request...
-(require_once __DIR__.'/../bootstrap/app.php')
+(require_once __DIR__ . '/../bootstrap/app.php')
     ->handleRequest(Request::capture());
